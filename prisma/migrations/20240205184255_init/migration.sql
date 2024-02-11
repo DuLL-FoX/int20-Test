d3fca338c19ac8a4c5fe11e2de48763990ebae12
DuLL_FoX <svarich.vadim@gmail.com>
2024-02-11 19:10:23 +0200

    Updated docker. Deleted the old migrations

--- prisma/migrations/20240205184255_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Status" AS ENUM ('ACTIVE', 'ENDED', 'CANCELLED');
-
--- AlterTable
-ALTER TABLE "auctions" ADD COLUMN     "status" "Status" NOT NULL DEFAULT 'ACTIVE';
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+npx prisma migrate dev --name init
 npx prisma migrate deploy
+npx prisma generate
 npm run start
